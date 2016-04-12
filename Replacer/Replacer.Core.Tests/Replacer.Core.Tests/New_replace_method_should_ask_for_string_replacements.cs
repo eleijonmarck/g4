2424606b87a7cfd80e3a752bb056db84adfbb77a
eleijonmarck <eric.leijonmarck@gmail.com>
2016-04-12 20:14:13 +0200

    added the repostories of the projectes

---
+++ Replacer/Replacer.Core.Tests/Replacer.Core.Tests/New_replace_method_should_ask_for_string_replacements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+//solution that the interviews will be presented for
+
+// try to test one replacer method
+namespace Replacer.Core.Tests
+{
+    [TestFixture]
+    public class New_replace_method_should_ask_for_string_replacements
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            var Replacer = new Replacer();
+        }
+    }
+
+    public class Replacer
+    {
+        string _textToReplace = File.ReadAllText("");
+        private string _bad;
+        private string _good;
+
+        public void Strings(string bad, string good)
+        {
+            _bad = bad;
+            _good = good;
+        }
+    }
+}
